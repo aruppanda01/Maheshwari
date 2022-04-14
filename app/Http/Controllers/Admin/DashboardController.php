44e309486cf0bc05453889aa4bb70e02ca377352
onenesstechs <arup.panda@onenesstechs.in>
2022-04-14 17:35:34 +0530

    Dashboard content changes

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Event;
 use App\Models\Forum;
+use App\Models\GoverningBody;
 use App\Models\Interest;
+use App\Models\Photo;
 use App\Models\Team;
+use App\Models\Update;
 use App\Models\User;
 
 class DashboardController extends Controller
@@ -19,6 +23,12 @@ class DashboardController extends Controller
     public function index()
     {
         $data = (object)[];
+        $data->totalUsers = User::count();
+        $data->events = Event::count();
+        $data->category = Category::count();
+        $data->photos = Photo::count();
+        $data->updates = Update::count();
+        $data->governing_bodies = GoverningBody::count();
 
         return view('admin.dashboard', compact('data'));
     }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,15 +1,15 @@
-{{-- test admin dashboaqrd. --}}
-
-
-
 @extends('admin.app')
 @section('title') Dashboard @endsection
 @section('content')
 
 <style type="text/css">
     .row-md-body.no-nav {
-    margin-top: 70px;
-}
+        margin-top: 70px;
+    } 
+    .section-mg a{
+        text-decoration: none;
+    }  
+
 </style>
 <div class="fixed-row">
     <div class="app-title">
@@ -19,5 +19,49 @@
     </div>
 </div>
     <div class="row section-mg row-md-body no-nav">
+        <div class="col-md-3 dash-card-col">
+            <a href="{{route('admin.user.index')}}">
+                <div class="card card-body mb-0">
+                    <h5 class="mb-2">Total User</h5>
+                    <p class="small mb-0">
+                        {{ $data->totalUsers }}
+                    </p>
+                    {{-- <i class="fas fa-list-alt app-menu__icon fa fa-group"></i> --}}
+                </div>
+            </a>
+        </div>
+        <div class="col-md-3 dash-card-col">
+            <a href="{{route('admin.category.index')}}">
+                <div class="card card-body mb-0">
+                    <h5 class="mb-2">Categories</h5>
+                    <p class="small mb-0">
+                        {{ $data->category }}
+                    </p>
+                    {{-- <i class="fas fa-list-alt app-menu__icon fa fa-group"></i> --}}
+                </div>
+            </a>
+        </div>
+        <div class="col-md-3 dash-card-col">
+            <a href="{{route('admin.event.index')}}">
+                <div class="card card-body mb-0">
+                    <h5 class="mb-2">Events</h5>
+                    <p class="small mb-0">
+                        {{ $data->events }}
+                    </p>
+                    {{-- <i class="fas fa-list-alt app-menu__icon fa fa-group"></i> --}}
+                </div>
+            </a>
+        </div>
+        <div class="col-md-3 dash-card-col">
+            <a href="{{route('admin.update.index')}}">
+                <div class="card card-body mb-0">
+                    <h5 class="mb-2">Updates</h5>
+                    <p class="small mb-0">
+                        {{ $data->updates }}
+                    </p>
+                    {{-- <i class="fas fa-list-alt app-menu__icon fa fa-group"></i> --}}
+                </div>
+            </a>
+        </div>
     </div>
 @endsection
